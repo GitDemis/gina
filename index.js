8f63dd67225ae61559d7b50de4f3df449235cf31
GitDemis <76735735+GitDemis@users.noreply.github.com>
2022-09-30 15:23:56 -0300

    8080

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const server = http.createServer((req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 server.listen(PORT, () => console.log('server running on port ${PORT}'));
